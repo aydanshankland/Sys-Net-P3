439bdd762cb7f9cee5b627bb089056fa29d8fb4c
KevinAlbee <123014587+kra25@users.noreply.github.com>
2025-04-07 15:01:45 -0500

    Done

--- client.cpp
+++ client.cpp
@@ -1,55 +1,53 @@
 #include "tcpClient.hpp"
-
 #include <iostream>
 
 #define PORTNUM 60001
 
 using namespace std;
 
+bool expectsInput(const string& message) {
+    return message.find("Username:") != string::npos ||
+           message.find("Password:") != string::npos ||
+           message.find("Enter") != string::npos ||
+           message.find("Press") != string::npos ||
+           message.find("Type") != string::npos ||
+           message.find("Invalid input") != string::npos ||
+           message.find("Successfully") != string::npos ||
+           message.find("logged out") != string::npos ||
+           message.find("Subscribed") != string::npos ||
+           message.find("not subscribed") != string::npos ||
+           message.find("locations:") != string::npos;
+}
 
 int main() {
-
     std::string serverAddress;
     std::cout << "Enter hostname: ";
     std::cin >> serverAddress;
 
-    if(serverAddress == "localhost"){
+    if(serverAddress == "localhost") {
         serverAddress = "127.0.0.1";
     }
 
-
-
     TCPClient client(PORTNUM, serverAddress);
 
-    std::string input, response;
+    std::string input;
+    std::string response;
 
-    // Initial server welcome message
     while (true) {
-        response = client.receiveData();
-        if (response == "EXIT") break;
+        // Keep receiving until something expects a user response
+        do {
+            response = client.receiveData();
+            if (response == "EXIT") {
+                std::cout << "Client Exiting.\n";
+                return 0;
+            }
 
-        std::cout << response;
+            std::cout << response;
+        } while (!expectsInput(response));
 
-        // Only wait for user input if server prompt expects it
-        if (response.find("Username:") != std::string::npos ||
-            response.find("Password:") != std::string::npos ||
-            response.find("Enter") != std::string::npos ||
-            response.find("Press") != std::string::npos ||
-            response.find("Type") != std::string::npos ||
-            response.find("Invalid input") != std::string::npos ||
-            response.find("Subscribed") != std::string::npos ||
-            response.find("not subscribed") != std::string::npos ||
-            response.find("Successfully") != std::string::npos ||
-            response.find("logged out") != std::string::npos ||
-            response.find("locations:") != std::string::npos
-        ) {
-            std::getline(std::cin >> std::ws, input);
-            client.sendData(input);
-        }
+        std::getline(std::cin >> std::ws, input);
+        client.sendData(input);
     }
-
-    std::cout << "[Client] Exiting.\n";
-
 
     return 0;
 }
